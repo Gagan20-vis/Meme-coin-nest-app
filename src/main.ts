7f88b8636aaf83e3340a806f8561e329a5523299
Gagan20-vis <gaganvishwakarma46@gmail.com>
2025-01-15 15:15:12 +0530

    init

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { NestFactory, Reflector } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+    app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+    const config = new DocumentBuilder()
+        .setTitle('AgentX')
+        .setDescription('The AgentX API description')
+        .setVersion('0.1')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
+    const port = process.env.PORT || 3000;
+
+    await app.listen(port);
+}
+bootstrap();
